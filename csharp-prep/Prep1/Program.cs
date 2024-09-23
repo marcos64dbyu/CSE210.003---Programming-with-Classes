58587b47829c6b3fc14f5f7d99482f0d1c8eaa17
Samuelpeter001 <Samuelpeter61@gmail.com>
2024-09-23 07:59:19 +0100

    Finished prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string first = console.Readline();
+        
+        Console.Write("What is your last name? ");
+        string last = console.Readline();
+
+        Console.Writeline($" Your name is {last}, {first} {last}.);
+        
+        
     }
 }
