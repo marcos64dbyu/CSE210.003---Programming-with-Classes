692f5607d11cdc5be041c446d0463ccab9248814
marcos64dbyu <mer24031@bui.edu>
2025-07-05 13:48:41 -0500

    resolve task teacher

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        /* C# Programming Exercise 1—Variables, Input, and OutputLinks to an external site. */
+
+
+        // input first name and last name
+        Console.WriteLine("What is your first name? ");
+        string first_name = Console.ReadLine();
+
+        Console.WriteLine("What is your last name? ");
+        string last_name = Console.ReadLine();
+
+
+        // print text
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
     }
 }
--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,56 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your grade percentage? ");
+        string grade_string = Console.ReadLine();           // Read value writen in terminal
+        int grade = int.Parse(grade_string);                // Convert string in int value
+        int last_digit = grade % 10;                        // Get the last digit to select + o - 
+
+        //Variable
+        string letter = "";
+
+        // Asign letter depending score
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+
+        }
+
+
+        // Asign + o - depending the last number in score
+        if (last_digit >= 7 && grade < 81 && grade > 60)
+        {
+            letter += "+";
+        }
+        else if (last_digit < 3 && grade < 84 && grade > 60)
+        {
+            letter += "-";
+        }
+
+        // Show message depending the score
+        if (grade >= 70)
+        {
+            Console.WriteLine($"Congratulation, you passed!. Your score is {letter}");
+        }
+        else
+        {
+            Console.WriteLine($"Sorry, you failed. Your score is {letter}");
+        }
     }
 }
