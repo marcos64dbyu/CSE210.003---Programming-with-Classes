107d1ccfad77811ac6d062308343bdbc1afcaa49
marcos64dbyu <mer24031@bui.edu>
2025-07-05 20:25:32 -0500

    Updater Excersice 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,12 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
+        // make variables
         string insertValue = "";
         int number = 0;
         List<int> numbers = new List<int>();
+        List<int> smallPositiveNumber = new List<int>();
 
+        // Print
         Console.WriteLine("Enter a list of numbers, type 0 when finished.");
        
+        //Calculate
         do 
         {
             Console.WriteLine("Enter number: ");
@@ -18,13 +22,28 @@ class Program
             numbers.Add(number);
         } while (insertValue != "0");
 
-        int total = numbers.Sum();
+        double total = numbers.Sum();
+        numbers.Remove(0);
 
         Console.WriteLine($"The sum is: {total}");
-        Console.WriteLine($"The average is: {total / (numbers.Count - 1)}"); 
-        Console.WriteLine($"The largest number is: {numbers.Max()}"); 
-        Console.WriteLine($"The smallest positive number is: {}"); 
+        Console.WriteLine($"The average is: {(total / (numbers.Count)):F3}"); 
+        Console.WriteLine($"The largest number is: {numbers.Max()}");
 
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            if (numbers[i] > 0)
+            {
+                smallPositiveNumber.Add(numbers[i]);
+            }
+        }
 
+        Console.WriteLine($"The smallest positive number is: {smallPositiveNumber.Min()}"); 
+        Console.WriteLine($"The sorted list is: "); 
+
+        numbers.Sort();
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            Console.WriteLine(numbers[i]); 
+        }
     }
 }
