c8c3fa53136a3cfbab5ab283278bf72552023e85
Marcos64d <marco_merizalde_verduga@hotmail.com>
2025-07-05 19:07:29 -0500

    Update programs

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Try again variable
+        string try_again = "no";
+        do
+        {
+            /* Console.WriteLine("What is the magic number? ");
+            string magicNumberString = Console.ReadLine();          // Read value written in terminal
+            int magicNumber = int.Parse(magicNumberString);         // Convert string in int value
+            */
+
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 100);
+
+            string guessStringNumber;
+            int guessNumber;
+            int tries = 0;
+
+            do
+            {
+                Console.WriteLine("What is your guess? ");
+                guessStringNumber = Console.ReadLine();             // Read value written in terminal
+                guessNumber = int.Parse(guessStringNumber);         // Convert string in int value
+
+                if (magicNumber == guessNumber)
+                {
+                    Console.WriteLine($"You guessed it!, in {tries} tries.");
+                    Console.WriteLine("Do you want to play again? (yes/no)");
+                    try_again = Console.ReadLine().ToLower();       
+
+                }
+                else if (guessNumber < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                    tries++;
+                }
+                else
+                {
+                    Console.WriteLine("Lower");
+                    tries++;
+                }
+
+            } while (magicNumber != guessNumber);
+        } while (try_again == "yes");
     }
 }
--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        string insertValue = "";
+        int number = 0;
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+       
+        do 
+        {
+            Console.WriteLine("Enter number: ");
+            insertValue = Console.ReadLine();
+            number = int.Parse(insertValue);
+            numbers.Add(number);
+        } while (insertValue != "0");
+
+        int total = numbers.Sum();
+
+        Console.WriteLine($"The sum is: {total}");
+        Console.WriteLine($"The average is: {total / (numbers.Count - 1)}"); 
+        Console.WriteLine($"The largest number is: {numbers.Max()}"); 
+
     }
 }
