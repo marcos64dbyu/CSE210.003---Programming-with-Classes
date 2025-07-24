bda464fe3809b9cb62f4f7cd2b96d503756e28c4
Marco Merizalde <marcos64d@gmail.com>
2025-07-24 00:30:58 -0500

    Work with constructor in Fraction solution

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Net.NetworkInformation;
+using System.Xml.Schema;
+using Fractions;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction f1 = new Fraction();
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetTop());
+        Fraction f2 = new Fraction(5);
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetTop());
+        Fraction f3 = new Fraction(3, 4);
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());        
+        f3.SetTop(1);
+        f3.SetBottom(3);
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue()); 
     }
 }
